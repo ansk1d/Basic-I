5868ac8f2795f12f9e61ed1e492aff6f49702c71
ansk1d <105351282+ansk1d@users.noreply.github.com>
2022-06-05 22:39:46 +0300

    lets goo!

---
+++ For Loops: Basic I.py
@@ -0,0 +1,27 @@
+# for x in range(151):
+#     print(x)
+
+# for y in range(5,1001,5):
+#     print (y)
+
+# for z in range(101):
+#     if z%5==0:
+#         print("Coding")
+#     elif z%10==0:
+#         print("Coding Dojo")
+#     else:
+#         print(z)
+
+# sum = 0
+# for s in range(0,500000,1):
+#     sum += s
+# print (sum)
+
+# for count in range(2022,-1,-4):
+#     print (count)
+
+lowNum=2
+highNum=9
+mult=3
+for f in range(lowNum+1,highNum+1,mult):
+    print(f)
